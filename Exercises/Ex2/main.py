641b971b93f323221581c3b687c0f89877084b31
ananth1996 <ananth1996@gmail.com>
2019-10-11 16:34:52 +0300

    Ex2 solution docs and code commenting

--- Exercises/Ex2/main.py
+++ Exercises/Ex2/main.py
@@ -5,7 +5,7 @@ from sailing import SailingGridworld
 from itertools import product
 
 # Set up the environment
-env = SailingGridworld(rock_penalty=-10)
+env = SailingGridworld(rock_penalty=-2)
 value_est = np.zeros((env.w, env.h))
 env.draw_values(value_est)
 
@@ -29,7 +29,19 @@ if __name__ == "__main__":
     delta = 0
     iterations = 0 
 #%%
-    N =100
+    #* Code for Task 1
+    # N =100
+    # for _ in range(N):
+    #     for state in  product(range(env.w),range(env.h)):
+    #         v = value_est[state]
+    #         transitions = env.transitions[state]
+    #         action_values = get_action_values(transitions,gamma,value_est)
+    #         value_est[state] = action_values.max()
+    
+    #     env.draw_values(value_est)
+    #     env.render()
+    
+    #* Code for task 3 
     while True:
         delta=0
         for state in  product(range(env.w),range(env.h)):
@@ -40,13 +52,16 @@ if __name__ == "__main__":
             delta = max(delta,np.abs(v-max_action_value))
             value_est[state] = max_action_value
         iterations+=1
-        print(f"{delta:e}")
         if delta < epsilon:
             break
+        # env.cleartext()
         # env.draw_values(value_est)
         # env.render()
+    
     print(f"Value Iteration took {iterations} number of iterations")
 
+    # Policy Updating
+    # it is kept separate 
     for state in product(range(env.w),range(env.h)):
         transitions = env.transitions[state]
         action_values = get_action_values(transitions,gamma,value_est)    
@@ -58,7 +73,7 @@ if __name__ == "__main__":
     env.draw_actions(policy)
     env.render()
     sleep(1)
-    input("Something bro")
+    # input("Something bro")
     # Save the state values and the policy
     fnames = "values.npy", "policy.npy"
     np.save(fnames[0], value_est)
@@ -66,20 +81,30 @@ if __name__ == "__main__":
     print("Saved state values and policy to", *fnames)
 
     # Run a single episode
-    # TODO: Run multiple episodes and compute the discounted returns (Task 4)
-    done = False
-    while not done:
-        # Select a random action
-        # TODO: Use the policy to take the optimal action (Task 2)
-        # action = int(np.random.random()*4)
-        action = policy[state]
-        # Step the environment
-        state, reward, done, _ = env.step(action)
-
-        # Render and sleep
-        env.render()
-        # sleep(0.5)
-
+    #* Task 4
+    N = 1000
+    discounted_rewards = []
+    for _ in range(N):
+        done = False
+        G = 0 
+        step_count = 0
+        state = env.reset()
+        while not done:
+            # Select a random action
+            # * Task 2 as well
+            # action = int(np.random.random()*4)
+            action = policy[state]
+            # Step the environment
+            state, reward, done, _ = env.step(action)
+            G += np.power(gamma,step_count)*reward
+            # Render and sleep
+            # env.render()
+            # sleep(0.5)
+            step_count+=1
+        discounted_rewards.append(G)
+    
+    print(f"Mean of discounted rewards: {np.mean(discounted_rewards)}")
+    print(f"Std dev of discounted rewards: {np.std(discounted_rewards)}")
 
 
 #%%
